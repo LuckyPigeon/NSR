22ae9105d13dfd995157b97ccaa5f521b0bb9cc4
LuckyPigeon <lucky90322@gmail.com>
2020-01-04 20:05:23 +0800

    Update BFS crawling item structure

--- Crawler/NSR_BFS_Crawler/NSR_BFS_Crawler/items.py
+++ Crawler/NSR_BFS_Crawler/NSR_BFS_Crawler/items.py
@@ -13,6 +13,36 @@ class NsrBfsCrawlerItem(scrapy.Item):
     # name = scrapy.Field()
     pass
 
+class WikiItem(scrapy.Item):
+    key = scrapy.Field()
+    value = scrapy.Field()
+
+class InfoItem(scrapy.Item):
+    author = scrapy.Field()
+    publisher = scrapy.Field()
+    date = scrapy.Field()
+    lang = scrapy.Field()
+    price = scrapy.Field()
+    sale = scrapy.Field()
+    salePrice = scrapy.Field()
+    saleLimit = scrapy.Field()
+
+class BookItem(scrapy.Item):
+    title = scrapy.Field()
+    img = scrapy.Field()
+    url = scrapy.Field()
+    sale = scrapy.Field()
+    salePrice = scrapy.Field()
+
+class AbstractItem(scrapy.Item):
+    content = scrapy.Field()
+
+class DetailItem(scrapy.Item):
+    ISBN = scrapy.Field()
+    specification = scrapy.Field()
+    location = scrapy.Field()
+    classification = scrapy.Field()
+
 class CharacterItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
